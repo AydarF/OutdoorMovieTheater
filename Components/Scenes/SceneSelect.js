eda7163e765f32dea9a7c4fcd47d4d338fc08b56
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2018-01-24 17:10:03 -0500

    Create SceneSelect component

---
+++ Components/Scenes/SceneSelect.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, VrButton} from 'react-vr';
+
+import SceneSelectMenu from './Layouts/SceneSelectMenu.js';
+
+class SceneSelect extends React.Component {
+	render() {
+		return (
+          <SceneSelectMenu />
+		);
+	}
+};
