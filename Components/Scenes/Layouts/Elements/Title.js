f90a27ffb852bf178cb7dfbd29be01c6e4711d44
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2018-01-27 00:48:21 -0500

    Add animation to the main menu title

--- Components/Scenes/Layouts/Elements/Title.js
+++ Components/Scenes/Layouts/Elements/Title.js
@@ -1,16 +1,55 @@
 import React from 'react';
-import {Text, View, VrButton} from 'react-vr';
+import {Text, View, Animated} from 'react-vr';
+import { Easing } from 'react-native';
 
 class Title extends React.Component {
+  constructor() {
+    super();
+    this.state = {fade: new Animated.Value(1), slide: new Animated.Value(1.5)};
+  }
+
+  componentDidMount() {
+    Animated.sequence([
+      Animated.timing(
+        this.state.fade,
+        {
+          toValue: 0,
+          duration: 2000,
+          easing: Easing.ease
+        }
+      ),
+      Animated.stagger(500, [
+      Animated.timing(
+        this.state.fade,
+        {
+          toValue: 1,
+          duration: 2000,
+          easing: Easing.ease
+        }
+      ),
+      Animated.timing(
+        this.state.slide,
+        {
+          toValue: 0,
+          duration: 2000,
+          easing: Easing.ease
+        }
+      )
+     ]) 
+   ]).start();
+  }
+ 
   render() {
   	return (
       <View>
-        <Text style={{
+        <Animated.Text style={{
         	fontSize: 0.5,
         	fontWeight: '400',
         	textAlign: 'center',
-        	textAlignVertical: 'center'
-        }}>{this.props.text}</Text>
+        	textAlignVertical: 'center',
+          opacity: this.state.fade,
+          transform: [{translateY: this.state.slide}]
+        }}>{this.props.text}</Animated.Text>
       </View>
   	);
   }
